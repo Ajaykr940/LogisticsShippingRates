bc3d74eaedcdf03a02d5d6e7c5be37b6939691cb
Ajay <ajay.karri994@gmail.com>
2026-02-16 14:36:47 -0500

    added a new date feature to the file

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,7 +2,9 @@
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
+date =  str(input("enter month:"))
 ## Calculate shipping cost
 shipping_cost = weight * rate
+
 ## Display the result
-print(f"Shipping Cost: {shipping_cost} USD")
+print(f"Shipping Cost: {shipping_cost} USD for this date {date}")
